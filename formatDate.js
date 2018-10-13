93c23a16e11c92ef448da3337bf60bfce6c02457
Jarek Kajko <jaruto7@gmail.com>
2018-10-13 18:09:01 +0200

    Format time3

--- formatDate.js
+++ formatDate.js
@@ -1,27 +1,16 @@
 const formatDate = (time) => {
-    let hours = Math.floor(time / 3600);
-    let minutes = Math.floor((time - (hours * 3600)) / 60);
-    let seconds = time - (hours * 3600) - (minutes * 60);
-    console.log(time);
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time - (hours * 3600)) / 60);
+    const seconds = time - (hours * 3600) - (minutes * 60);
 
-switch(time) {
-    case seconds:
-        // console.log(`Uptime: ~ 0 sec`);
-        console.log(`Saturday`);
-        break;
-    case seconds:
-        // console.log(`Uptime: ~ ( ${seconds} ).toFixed( 0 ) sec`);
-        console.log(`Sunday`);
-        break;
-    case minutes:
-        // console.log(`Uptime: ~ ( ${minutes} ).toFixed( 0 ) min  ( ${seconds} ).toFixed( 0 ) sec`);
-        console.log(`Monday`);
-        break;
-    case hours:
-        // console.log(`Uptime: ~ ( ${hours} ).toFixed( 0 ) hours  ( ${minutes} ).toFixed( 0 ) min  ( ${seconds} ).toFixed( 0 ) sec`);
-        console.log(`Tuesday`);
-        break;
-    }  
+    if(seconds < 0) {
+        console.log(`Uptime: ~  ${seconds.toFixed( 0 )} sec`);
+    }else if(minutes > 0) {
+        console.log(`Uptime: ~ ${minutes.toFixed( 0 )} min ${seconds.toFixed( 0 )} sec`);
+    }else if(hours > 0) {
+        console.log(`Uptime: ~ ${hours.toFixed( 0 )} hours ${minutes.toFixed( 0 )} minutes ${seconds.toFixed( 0 )} + sec`);
+    }else if(seconds === 0){
+        console.log(`Uptime: ~ ${seconds.toFixed( 0 )} sec`);
+    }
 }
-
 module.exports = formatDate;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const os = require( 'os' );
 const formatDate = require( './formatDate' );
 const time = formatDate( os.uptime() );
  
-    formatDate(time);
+formatDate(time);
 
 
 
