bd531c7b2d12020a5563926eb6b13f54c73e6d54
Jarek Kajko <jaruto7@gmail.com>
2018-10-13 17:06:59 +0200

    Format time2

--- formatDate.js
+++ formatDate.js
@@ -1,20 +1,25 @@
 const formatDate = (time) => {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time - (hours * 3600)) / 60);
-    const seconds = time - (hours * 3600) - (minutes * 60);
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time - (hours * 3600)) / 60);
+    let seconds = time - (hours * 3600) - (minutes * 60);
+    console.log(time);
 
 switch(time) {
-    case seconds == 0:
-        console.log(`Uptime: ~ 0 sec`);
+    case seconds:
+        // console.log(`Uptime: ~ 0 sec`);
+        console.log(`Saturday`);
         break;
-    case seconds > 0:
-        console.log(`Uptime: ~ ( ${seconds} ).toFixed( 0 ) sec`);
+    case seconds:
+        // console.log(`Uptime: ~ ( ${seconds} ).toFixed( 0 ) sec`);
+        console.log(`Sunday`);
         break;
-    case minutes > 0:
-        console.log(`Uptime: ~ ( ${minutes} ).toFixed( 0 ) min  ( ${seconds} ).toFixed( 0 ) sec`);
+    case minutes:
+        // console.log(`Uptime: ~ ( ${minutes} ).toFixed( 0 ) min  ( ${seconds} ).toFixed( 0 ) sec`);
+        console.log(`Monday`);
         break;
-    case hours > 0:
-        console.log(`Uptime: ~ ( ${hours} ).toFixed( 0 ) hours  ( ${minutes} ).toFixed( 0 ) min  ( ${seconds} ).toFixed( 0 ) sec`);
+    case hours:
+        // console.log(`Uptime: ~ ( ${hours} ).toFixed( 0 ) hours  ( ${minutes} ).toFixed( 0 ) min  ( ${seconds} ).toFixed( 0 ) sec`);
+        console.log(`Tuesday`);
         break;
     }  
 }
