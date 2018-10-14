a110c0933e210fd1864042493dd9a8141232fb55
Jarek Kajko <jaruto7@gmail.com>
2018-10-14 20:11:23 +0200

    Format time4

--- formatDate.js
+++ formatDate.js
@@ -3,14 +3,14 @@ const formatDate = (time) => {
     const minutes = Math.floor((time - (hours * 3600)) / 60);
     const seconds = time - (hours * 3600) - (minutes * 60);
 
-    if(seconds < 0) {
+    if(seconds < 0 && seconds < 60) {
         console.log(`Uptime: ~  ${seconds.toFixed( 0 )} sec`);
     }else if(minutes > 0) {
         console.log(`Uptime: ~ ${minutes.toFixed( 0 )} min ${seconds.toFixed( 0 )} sec`);
     }else if(hours > 0) {
-        console.log(`Uptime: ~ ${hours.toFixed( 0 )} hours ${minutes.toFixed( 0 )} minutes ${seconds.toFixed( 0 )} + sec`);
+        console.log(`Uptime: ~ ${hours.toFixed( 0 )} hours ${minutes.toFixed( 0 )} minutes ${seconds.toFixed( 0 )} sec`);
     }else if(seconds === 0){
-        console.log(`Uptime: ~ ${seconds.toFixed( 0 )} sec`);
+        console.log(`Uptime: ~ 0 sec`);
     }
 }
 module.exports = formatDate;
